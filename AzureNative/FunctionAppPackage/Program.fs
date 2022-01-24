2f33445192731eef8166ef5cc59817a203e6f8e1
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-01-24 21:29:27 +0100

    Replace wrong Echo API and output test URL

--- AzureNative/FunctionAppPackage/Program.fs
+++ AzureNative/FunctionAppPackage/Program.fs
@@ -3,6 +3,7 @@
 open Pulumi.FSharp.AzureNative.Components.FunctionAppPackage
 open Pulumi.FSharp.Config
 open Pulumi.FSharp
+open Pulumi
 
 Deployment.run (fun () ->
     let functionAppInfrastructure =
@@ -12,5 +13,5 @@ Deployment.run (fun () ->
             projectPackagePublishPath config.["projectPublishPath"]
         }
     
-    dict []
+    dict [ "Test URL", Output.Format($"https://{functionAppInfrastructure.App.DefaultHostName}/api/Echo?test") :> obj ]
 )
